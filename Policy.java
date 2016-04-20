9368eeddb4c9e3d337eec32138bb757464ae8673
Michael Manning <netitrinr@gmail.com>
2016-04-19 23:07:25 -0600

    Started on the evaluate function

--- Policy.java
+++ Policy.java
@@ -9,6 +9,10 @@ import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
 import org.jnetpcap.packet.PcapPacket;
 import org.jnetpcap.packet.PcapPacketHandler;
+import org.jnetpcap.protocol.tcpip.Tcp;
+import org.jnetpcap.protocol.tcpip.Udp;
+import org.jnetpcap.protocol.network.Ip4;
+import org.jnetpcap.packet.format.FormatUtils;
 
 public class Policy{
 
@@ -70,11 +74,64 @@ public class Policy{
         }
     }
 
+    /**
+     * checks to see if the packet passes the policy
+     * @param  packet jnetpcap PcapPacket
+     * @return        returns true if passes firewall policy
+     */
     public boolean evaluate(PcapPacket packet){
-        //Do stuff
+        boolean safe = true;
+        // Parse the packet in any way to bable to quickly check agaisnt the policy below.
+        Tcp tcp = new Tcp();
+        Udp udp = new Udp();
+        Ip4 ip = new Ip4();
+        String sourceIP = "";
+        String destIP = "";
+        int port = -1;
+        if(!packet.hasHeader(ip)){
+            return false;
+        }
+        if(packet.hasHeader(tcp)){
+            sourceIP = FormatUtils.ip(packet.getHeader(ip).source());
+            destIP = FormatUtils.ip(packet.getHeader(ip).source());
+            port = tcp.source();
+        }else if(packet.hasHeader(udp)){ // ???
+            port = udp.source(); // ???
+        }
 
+        if(host_port != "any" && host != destIP){ // Ensure packet doesn't have this port
+            safe = false;
+        }
+        if(attacker_port != "any" && Integer.parseInt(attacker_port) != port && port <= 65535 && port >= 0){ // Ensure packet doesn't have attacker port
+            safe = false;
+        }
+        if(attacker != "any" && attacker != sourceIP){ // Ensure packet doesn't have this attacker ip
+            safe = false;
+        }
+        if(stateful){
 
-        return false;
+        }else{ // Stateless
+
+            if(this.proto == "tcp" && !packet.hasHeader(tcp)){
+                safe = false;
+            }else if(this.proto == "udp" && !packet.hasHeader(udp)){
+                safe = false;
+            }else{
+                safe = false;
+            }
+            if(from_host.size() > 0){
+                for(String regex : from_host){ // Run regex on message body and flags
+
+                }
+            }
+            if(to_host.size() > 0){
+                for(String regex : to_host){ // Run regex on message body and flags
+
+                }
+            }
+        }
+
+        return safe;
     }
 
     @Override
